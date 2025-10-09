9f00a2b20e84f992a27f363e1c608dfebb03043f
cors_576 <you@example.com>
2025-10-09 20:21:02 +0300

    CR, api_v1

---
+++ api_v1/products/views.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.db_models import db_helper
+
+from . import crud
+from .schemas import Product, ProductCreate
+
+
+router = APIRouter(tags=["Products"])
+
+
+@router.get("/", response_model=list[Product])
+async def get_products(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    await crud.get_products(session=session)
+
+
+@router.post("/", response_model=Product)
+async def create_product(
+    product_in: ProductCreate,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    return await crud.create_product(session=session, product_in=product_in)
+
+
+@router.get("/{product_id}/", response_model=Product)
+async def get_product_by_id(
+    product_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    product = await crud.get_product(session=session, product_id=product_id)
+    if product is not None:
+        return product
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {product_id} not found!,",
+    )
+
+
+# КАК ДАННЫЕ ПЕРЕДАДУТСЯ В product:PYDANTIC ДЛЯ ПАРСИНГА?
---
+++ core/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class Setting(BaseSettings):
+    api_v1_prefix = "/api/v1"
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/./shop.db"
+    # db_echo: bool = False
+    db_echo: bool = True
+
+
+settings = Setting()
